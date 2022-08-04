6d7b2b7a4d9b823b95df37636776b94d2b5ec191
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-04 12:54:09 +0600

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,15 @@
+
+var firstName = prompt("Enter the first Name : ")
+var lastName = prompt("Enter Your Last name")
+
+
+var fullName = firstName + lastName;
+
+
+document.write("Fullname is : " + fullName + "<br/>")
+
+
+document.write("Length of full name : " + fullName.length + "<br/>")
+
+
+document.write("Uppercase Full name : " + fullName.toUpperCase())
