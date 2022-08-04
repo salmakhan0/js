5277e0ff4238ccf204fb755faf80b4a87e1e11db
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-04 12:58:50 +0600

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,24 @@
+
+var num1, num2, sum, sub, mul, div, rem;
+num1 = parseInt(prompt("Enter First Number : "))
+num2 = parseInt(prompt("Enter Second Number : "))
+
+
+sum = num1 + num2
+document.write("Addition = " + sum + "<br/>")
+
+
+sub = num1 - num2
+document.write("subtraction = " + sub + "<br/>")
+
+
+mul = num1 * num2
+document.write("Multiplication = " + mul + "<br/>")
+
+
+div = num1 / num2
+document.write("Division = " + div + "<br/>")
+
+
+rem = num1 % num2
+document.write("Remainder = " + rem + "<br/>")
