f8a305ffd393b16ed0a7dfe8db18938ac9fd1172
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-05 23:48:01 +0600

    Add files via upload

---
+++ index4.js
@@ -0,0 +1,19 @@
+
+
+var marks = parseInt(prompt("Enter Marks : "))
+
+
+if (marks >= 80)
+    document.write("A+")
+else if (marks >= 70)
+    document.write("A")
+else if (marks >= 60)
+    document.write("A-")
+else if (marks >= 50)
+    document.write("B")
+else if (marks >= 40)
+    document.write("C")
+else if (marks >= 33)
+    document.write("D")
+else
+    document.write("Sorry. You are Failed")
