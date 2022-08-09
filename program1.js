821b4683314ab9e8dd02b7e511498b17f411aea4
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-09 21:32:45 +0600

    Add files via upload

---
+++ program1.js
@@ -0,0 +1,8 @@
+
+document.write("Welcome to JS Program");
+document.write("<h1>Welcome to JS Program</h1>");
+document.write("<h2>Welcome to JS Program</h2>");
+
+console.log("Welcome to JS Program");
+
+alert("welcome to js program");
