9066c35614ff6ee0bd2b4ed83b21edc5fa35b9ec
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-11 01:15:58 +0600

    Add files via upload

---
+++ p11.js
@@ -0,0 +1,32 @@
+
+var num1 = parseInt(prompt("Enter first numebr : "));
+var num2 = parseInt(prompt("Enter second numebr : "));
+
+
+if (num1 > num2) {
+  console.log("Large Number is : " + num1);
+}
+if (num1 < num2) {
+  console.log("Large Number is : " + num2);
+}
+if (num1 == num2) {
+  console.log("Equal numbers");
+}
+
+
+if (num1 > num2) {
+  console.log("Large Number is : " + num1);
+} else if (num1 < num2) {
+  console.log("Large Number is : " + num2);
+} else if (num1 == num2) {
+  console.log("Equal numbers");
+}
+
+
+if (num1 > num2) {
+  console.log("Large Number is : " + num1);
+} else if (num1 < num2) {
+  console.log("Large Number is : " + num2);
+} else {
+  console.log("Equal numbers");
+}
