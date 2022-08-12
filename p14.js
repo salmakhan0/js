bd949e2ac67bc5223475548653b25c9781b27a17
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-12 23:32:22 +0600

    Add files via upload

---
+++ p14.js
@@ -0,0 +1,12 @@
+for (var i = 1; i <= 10; i++) {
+  document.write("Bangladesh" + "<br>");
+}
+
+document.write("End of for loop");
+
+
+var sum = 0;
+for (var x = 1; x <= 100; x++) {
+  sum = sum + x;
+}
+document.write("The sum is " + sum);
