4b9a3beb6f2dcf5f696ea4a56bf853ee76c845dd
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-15 01:06:42 +0600

    Add files via upload

---
+++ p2.js
@@ -0,0 +1,21 @@
+console.log(typeof 123);
+console.log(typeof 123.5);
+console.log(typeof "123");
+console.log(typeof true);
+console.log(typeof false);
+
+
+var car;
+var car = ""; 
+
+var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
+person = null; 
+
+var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
+person = undefined; 
+
+typeof "John"; 
+typeof 3.14; 
+typeof true; 
+typeof false; 
+typeof x; 
