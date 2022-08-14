9b8d4bd39b0234c92fc893453c84460a2ce06c11
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-15 00:42:51 +0600

    Add files via upload

---
+++ p17.js
@@ -0,0 +1,9 @@
+for (var i = 1; i <= 100; i++) {
+  if (i == 20) break;
+  document.write(i + "<br/>");
+}
+
+for (var i = 1; i <= 100; i++) {
+  if (i == 20) continue;
+  document.write(i + "<br/>");
+}
