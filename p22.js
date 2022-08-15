482df6641771d7a8dbd974c9317cb1594863767a
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-16 01:42:37 +0600

    Add files via upload

---
+++ p22.js
@@ -0,0 +1,8 @@
+var names = ["Mina", "Rabeya", "Kolpona", "Anis"];
+console.log(names);
+
+var numbers = [20, 5, 25, 45, 1];
+numbers.sort(function (a, b) {
+  return b - a;
+});
+console.log(numbers);
