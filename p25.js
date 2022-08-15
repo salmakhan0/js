6c2efc384b1420a7719a81faaab07ed6e7d09074
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-16 02:11:05 +0600

    Add files via upload

---
+++ p25.js
@@ -0,0 +1,23 @@
+console.log(Math.sqrt(25));
+
+console.log(Math.sin(25));
+
+console.log(Math.abs(-25));
+
+console.log(Math.pow(2, 3));
+
+console.log(Math.floor(2.4));
+console.log(Math.ceil(2.4));
+
+console.log(Math.round(2.4));
+
+console.log(Math.max(2, 8));
+console.log(Math.min(2, 8));
+
+console.log(Math.random());
+
+console.log(Math.random() * 6);
+
+console.log(Math.floor(Math.random() * 6));
+
+console.log(Math.floor(Math.random() * 5) + 1);
