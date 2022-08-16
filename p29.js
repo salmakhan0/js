01a22322bf25e6ae7320db5a8d8d46bb0396342b
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-17 01:15:07 +0600

    Add files via upload

---
+++ p29.js
@@ -0,0 +1,8 @@
+var name = "Anisul Islam";
+var age = 29;
+var cgpa = 3.92;
+var lineBreak = "<br/>";
+
+document.write("Name : " + name + lineBreak);
+document.write("Age : " + age + lineBreak);
+document.write("CGPA : " + cgpa + lineBreak);
