72d789daeb34ecc473aaffbb6d28e18f5feb8d0a
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-18 16:17:23 +0600

    Add files via upload

---
+++ p33.js
@@ -0,0 +1,12 @@
+var fName = "Salma";
+var lName = "Khan";
+
+var fullName = fName + " " + lName;
+console.log(fullName);
+console.log("Today is" + " a " + "beautiful day");
+console.log("My name is " + fullName);
+
+var num1 = 20;
+var num2 = 30;
+var sum = num1 + num2;
+console.log(num1 + " + " + num2 + " = " + sum);
