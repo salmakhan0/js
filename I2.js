e193989540ec758676f79d16498566a5d8359440
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-09-09 03:23:16 +0600

    Add files via upload

---
+++ I2.js
@@ -0,0 +1,15 @@
+fullName = "anSalma kh";
+console.log(fullName);
+var fullName; // valid
+
+x = 25;
+console.log(x);
+let x; // reference error
+
+y = 10;
+console.log(y);
+const y; // syntax error
+
+// The purpose of "use strict" is to indicate that the code should be executed in "strict mode".
+"use strict";
+x = 3.14;  
---
+++ I3.js
@@ -0,0 +1,11 @@
+var x = 20;
+var y = 10;
+var sum = x + y;
+console.log(`sum is ${x+y}`);
+console.log(`sum is ${sum}`);
+
+let uniName = `This is a template literal`
+
+let fullName = "Salma Khan";
+let message = `I am ${fullName}. I am teaching ES6`;
+console.log(message);
