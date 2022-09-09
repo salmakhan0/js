23ada180e41a25031871b832b958dd604a6ee01f
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-09-09 23:15:00 +0600

    Add files via upload

--- p2.js
+++ p2.js
@@ -1,21 +1,73 @@
-console.log(typeof 123);
-console.log(typeof 123.5);
-console.log(typeof "123");
-console.log(typeof true);
-console.log(typeof false);
+// 4 ways to call api - XMLHttpRequest, fetch, axios, jquery
 
+// fetch() has replaced XMLHttpRequest
+// fetch() - global method for making HTTP Request
+// 2 ways to call - then, async await
 
-var car;
-var car = ""; 
+// + fetch() is easy to use compare to XMLHttpRequest
+// + fetch() returns a promise
+// - returned promise can only handle network error
+// - does not support all the older browser
 
-var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
-person = null; 
+// method for making HTTP Request
+const makeRequest = async (url, config) => {
+  const res = await fetch(url, config);
+  if (!res.ok) {
+    const message = `Error : ${res.status}`;
+    throw new Error(message);
+  }
+  const data = await res.json();
+  return data;
+};
 
-var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
-person = undefined; 
+const deleteData = () => {
+  makeRequest("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "DELETE",
+  })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
 
-typeof "John"; 
-typeof 3.14; 
-typeof true; 
-typeof false; 
-typeof x; 
+deleteData();
+
+// const updateData = () => {
+//   makeRequest("https://jsonplaceholder.typicode.com/posts/1", {
+//     method: "PATCH",
+//     body: JSON.stringify({
+//       title: "foomaraarrara",
+//     }),
+//     headers: {
+//       "Content-type": "application/json; charset=UTF-8",
+//     },
+//   })
+//     .then((res) => console.log(res))
+//     .catch((err) => console.log(err));
+// };
+
+// updateData();
+
+// const sendData = () => {
+//   makeRequest("https://jsonplaceholder.typicode.com/posts", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       title: "foo",
+//       body: "bar",
+//       userId: 1,
+//     }),
+//     headers: {
+//       "Content-type": "application/json; charset=UTF-8",
+//     },
+//   })
+//     .then((res) => console.log(res))
+//     .catch((err) => console.log(err));
+// };
+
+// sendData();
+
+// const getData = () => {
+//   makeRequest("https://jsonplaceholder.typicode.com/posts")
+//     .then((res) => console.log(res))
+//     .catch((err) => console.log(err));
+// };
+
+// getData();
